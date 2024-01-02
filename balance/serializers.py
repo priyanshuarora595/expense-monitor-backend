40a1b2b6c8ab8c8ed3c305c3aadb4ee5ec638e19
Priyanshu Arora <priyanshu@gkmit.co>
2024-01-02 14:51:58 +0530

    added month format in balance

--- balance/serializers.py
+++ balance/serializers.py
@@ -5,6 +5,7 @@ from balance.models import Balance
 from transactions.models import Sources
 from transactions.serializers import SourcesSerializer
 
+
 class BalanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Balance
@@ -15,10 +16,11 @@ class BalanceSerializer(serializers.ModelSerializer):
             "month",
             "source",
             "first_day_amount",
-            "last_day_amount"
+            "last_day_amount",
         ]
-        
+
     def to_representation(self, obj):
         ret = super(BalanceSerializer, self).to_representation(obj)
         ret["source"] = SourcesSerializer(Sources.objects.get(id=ret["source"])).data
+        ret["month"] = str(ret["month"]).zfill(2)
         return ret
