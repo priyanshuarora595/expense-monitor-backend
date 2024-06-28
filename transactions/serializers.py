ab406433f1a9634454990c1d547983426c95751e
Jeetesh Sisodia <priyanshu@gkmit.co>
2024-06-28 14:03:53 +0530

    add is_active attribute to source serializer

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.utils import timezone
-from transactions.models import Transactions,Commodity,Sources,InternalTransactions
+from transactions.models import Transactions, Commodity, Sources, InternalTransactions
 
 
 class TransactionsSerializer(serializers.ModelSerializer):
@@ -16,18 +16,20 @@ class TransactionsSerializer(serializers.ModelSerializer):
             "commodity",
             "comments",
         ]
-        
-        extra_kwargs = {'user': {'write_only': True}}
-        
+
+        extra_kwargs = {"user": {"write_only": True}}
+
     def to_representation(self, obj):
         ret = super(TransactionsSerializer, self).to_representation(obj)
-        ret["commodity"] = CommoditySerializer(Commodity.objects.get(id=ret["commodity"])).data
+        ret["commodity"] = CommoditySerializer(
+            Commodity.objects.get(id=ret["commodity"])
+        ).data
         ret["source"] = SourcesSerializer(Sources.objects.get(id=ret["source"])).data
         obj.date = timezone.localtime(obj.date)
-        ret['date'] = obj.date.strftime('%Y-%m-%d %H:%M')
+        ret["date"] = obj.date.strftime("%Y-%m-%d %H:%M")
         return ret
-    
-    
+
+
 class InternalTransactionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = InternalTransactions
@@ -40,55 +42,50 @@ class InternalTransactionsSerializer(serializers.ModelSerializer):
             "amount",
             "comments",
         ]
-        
-        extra_kwargs = {'user': {'write_only': True}}
-        
+
+        extra_kwargs = {"user": {"write_only": True}}
+
     def to_representation(self, obj):
         ret = super(InternalTransactionsSerializer, self).to_representation(obj)
         ret["source"] = SourcesSerializer(Sources.objects.get(id=ret["source"])).data
-        ret["destination"] = SourcesSerializer(Sources.objects.get(id=ret["destination"])).data
+        ret["destination"] = SourcesSerializer(
+            Sources.objects.get(id=ret["destination"])
+        ).data
         obj.date = timezone.localtime(obj.date)
-        ret['date'] = obj.date.strftime('%Y-%m-%d %H:%M')
+        ret["date"] = obj.date.strftime("%Y-%m-%d %H:%M")
         return ret
-    
+
+
 class CommoditySerializer(serializers.ModelSerializer):
     class Meta:
         model = Commodity
-        fields = [
-            "id",
-            'user',
-            "name"
-        ]
-        extra_kwargs = {'user': {'write_only': True}}
-        
+        fields = ["id", "user", "name"]
+        extra_kwargs = {"user": {"write_only": True}}
+
     def to_representation(self, obj):
         ret = super(CommoditySerializer, self).to_representation(obj)
         # del ret['user']
         return ret
-        
+
+
 class SourcesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sources
-        fields = [
-            "id",
-            'user',
-            "name",
-            'default'
-        ]
-        extra_kwargs = {'user': {'write_only': True}}
-        
+        fields = ["id", "user", "name", "default", "is_active"]
+        extra_kwargs = {"user": {"write_only": True}}
+
     def to_representation(self, obj):
         ret = super(SourcesSerializer, self).to_representation(obj)
         # del ret['user']
         return ret
-    
+
     def save(self, **kwargs):
         request = self.context.get("request")
         request_data = request.data
-        if request_data['default']=='true':
-            user = request_data['user']
+        if request_data["default"] == "true":
+            user = request_data["user"]
             sources = Sources.objects.filter(user=user)
             for source in sources:
-                source.default=False
+                source.default = False
                 source.save()
         return super().save(**kwargs)
