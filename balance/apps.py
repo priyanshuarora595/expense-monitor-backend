80af97cd3c06be852d557490dade9f703e246132
Jeetesh Sisodia <priyanshu@gkmit.co>
2024-06-28 17:51:56 +0530

    remove scheduler

--- balance/apps.py
+++ balance/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class BalanceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'balance'
-    def ready(self):
-        from scheduler import start
-        start()
+    # def ready(self):
+    #     from scheduler import start
+    #     start()
