86ed950f8f66b73f8b548b03f3b360eef66b6a4d
Priyanshu Arora <priyanshuarora595@gmail.com>
2026-09-17 17:15:47 +0530

    reduced unnecessary requirements

--- api/views.py
+++ api/views.py
@@ -1,31 +1,35 @@
 from rest_framework.views import APIView
-import rest_framework
+from rest_framework.utils.serializer_helpers import ReturnDict
 
 from django.http import HttpResponse
 from dotenv import load_dotenv
 from io import BytesIO as IO
-import pandas as pd
+import xlsxwriter
 
 
 load_dotenv()
 
 
-def column_is_type(col, t):
-    return col.apply(type).eq(t).all()
-
-
 class ExportView(APIView):
     def process_data(self, data):
-        df = pd.DataFrame(data)
-        df = df.drop(columns=["id"])
-        for col in df.columns:
-            if column_is_type(df[col], rest_framework.utils.serializer_helpers.ReturnDict):
-                df[col] = df[col].apply(lambda x: x.get("name"))
-        print(df)
+        columns = [key for key in data[0].keys() if key != "id"]
+
+        for col in columns:
+            if all(isinstance(row[col], ReturnDict) for row in data):
+                for row in data:
+                    row[col] = row[col].get("name")
+
         excel_file = IO()
-        xlwriter = pd.ExcelWriter(excel_file, engine='xlsxwriter')
-        df.to_excel(excel_writer=xlwriter, sheet_name='sheetname',index=False)
-        xlwriter.close()
+        workbook = xlsxwriter.Workbook(excel_file, {"in_memory": True})
+        worksheet = workbook.add_worksheet("sheetname")
+
+        for col_idx, col in enumerate(columns):
+            worksheet.write(0, col_idx, col)
+        for row_idx, row in enumerate(data, start=1):
+            for col_idx, col in enumerate(columns):
+                worksheet.write(row_idx, col_idx, row[col])
+
+        workbook.close()
         excel_file.seek(0)
         response = HttpResponse(excel_file.read(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         # set the file name in the Content-Disposition header
